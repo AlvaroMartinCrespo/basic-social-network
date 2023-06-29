8b9e924a3a9587db2ceab507735bdc52243942cf
Alvaro <alvaro.martin.crespo.00@gmail.com>
2023-06-29 12:07:49 +0200

    session

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,4 +1,7 @@
+import { useSession } from 'next-auth/react';
 export default function Dashboard() {
+  const { data: session } = useSession();
+  console.log(session);
   return (
     <section className="flex flex-col items-center justify-center ">
       <h1 className="text-5xl font-bold">Dashboard</h1>
